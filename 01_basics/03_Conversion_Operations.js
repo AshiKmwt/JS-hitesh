c31ab5b4d37e591d6238e139b6b46c4f81856cce
aashika5 <160109373+AshiKmwt@users.noreply.github.com>
2025-05-26 19:26:24 +0530

    conversion file addded

---
+++ 01_basics/03_Conversion_Operations.js
@@ -0,0 +1,42 @@
+// let score = "55ab";
+// let score = null;
+// let score= undefined; //NAN-not a number
+// let score = true;
+// let score = false;
+let score = "ashika";
+
+
+console.log(typeof score)
+console.log(typeof (score))
+
+
+let valueInNumber = Number(score); //conversion from str to num
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+
+//"33" - 33
+//"33abc" - NaN
+//true - 1 ; false - 0
+
+
+let isLoggedIn = "ashika"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 - true
+//0 - false
+// "" - false
+// "ashika" - true
+
+
+let someNumber = 55
+
+let cnvrtStr = String(someNumber)
+console.log(cnvrtStr);
+console.log(typeof cnvrtStr);
+
+
+
+//Investigation will continue with all datatypes
