a90fafee4c296c977a4d9323e954d1af428c0632
aashika5 <160109373+AshiKmwt@users.noreply.github.com>
2025-05-26 11:20:07 +0530

    adding more in datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,3 +1,78 @@
 "use strict"; //treat all JS code as newer version
 
-alert(3+3)
+
+//alert("hello ashika")
+//alert(3+3)              //we are using node.js not browsers
+
+
+//In coding ,the most imp thing is Readability
+//Example:
+console.log(5
+    +5); console.log("ashika")  //this is not a good practice
+
+
+let name = "ashika";
+let age = 5;
+let isLoggedIN = false;
+let state;
+
+
+
+//  number => 2 to power 53
+//  bigint => used to store big numbers as on trading websites or facebook, we don't use this much in our journey
+//  string => ""
+//  boolean => true/false  
+//  null => standalone value 
+//  undefined => value not assigned;variable made
+//  symbol => unique (used mostly in react and figma)
+
+
+
+//  object
+
+console.log(typeof "ashika");
+console.log(typeof age);
+console.log(typeof null); //object 
+console.log(typeof undefined);
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
