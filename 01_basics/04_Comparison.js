203c97e479cad24454cd5b3b177128608d3097b3
aashika5 <160109373+AshiKmwt@users.noreply.github.com>
2025-05-31 11:02:48 +0530

    push more code

--- 01_basics/04_Comparison.js
+++ 01_basics/04_Comparison.js
@@ -19,7 +19,7 @@ console.log(undefined < 0);
 console.log(undefined > 0);
 
 
-//Comarsion (>,<,<=,>=) and Equality (==,===) works differently in js
+//Comparison (>,<,<=,>=) and Equality (==,===) works differently in js
 
-
+d
 console.log("2" === 2);  //will check values and datatype too
