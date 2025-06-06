82e188aaa6aa16de9c5835bad1d7ce4dae419870
aashika5 <160109373+AshiKmwt@users.noreply.github.com>
2025-06-06 12:50:38 +0530

    add numbers and maths.js

--- 01_basics/06_Strings.js
+++ 01_basics/06_Strings.js
@@ -7,7 +7,7 @@ console.log(name + repo  + "on github");  //not recommended cause outdated
 
 console.log(`Hello my name is ${name} and my repo Count is ${repo}`); //string interpolation
 
-const gameName = new String("ashika-kmwt")
+const gameName = new String("ashika-kmwt-com")
 console.log(gameName);
 
 console.log(gameName[9])
@@ -36,6 +36,29 @@ console.log(url.replace("%55", "_"));
 
 console.log(url.includes("%55"));
 console.log(url.includes("%055"));
+
+
+console.log(gameName.split("-"));
+console.log(gameName.split("c"));
+
+console.log(gameName.at(-15));      //String("ashika-kmwt-com")
+console.log(gameName.at(14));  
+
+console.log(gameName.charCodeAt(2));     
+console.log(gameName.codePointAt(6)); 
+
+console.log(gameName.padEnd());
+console.log(gameName.padStart());
+
+console.log(gameName.repeat());
+console.log(gameName.valueOf());
+
+
+
+
+
+
+
 
 
 
---
+++ 01_basics/07_Nums_maths.js
@@ -0,0 +1,67 @@
+// const score = 555;
+// console.log(score );
+
+// const bal = new Number (100)  //new object me define kar raha hu, number type ka ,so it's a new way to store values as js objects.
+// console.log(bal);
+
+// console.log(bal.toString());
+// console.log(bal.toString().length);
+
+// console.log(bal.toFixed(2));    //mainly used in e-commerece app, decimal ke baad kitne value dikhana chahte hai clients ko ,
+// console.log(bal.toFixed(1));
+
+
+// const otherNum = 125.8765
+// console.log(otherNum.toPrecision(4));
+
+
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN')); //India
+
+
+
+// //note- Prototypes are properties and methods (hidden)
+
+
+
+
+//+++++++++++++++++++++++  Maths  ++++++++++++++++++++++
+
+console.log(Math);
+
+console.log(Math.abs(-4));  //-ve +ve me convert ho jata hai lekin +ve +ve hi rhta hai
+
+console.log(Math.round(5.6));        //round off the value
+console.log(Math.ceil(4.2));        //ceiling (upar hoti hai), so Top Value
+console.log(Math.floor(4.9));       //Floor(neeche hota hai), so Lower Value
+
+console.log(Math.min(5,2,6,7));
+console.log(Math.max(5,2,6,7));
+
+
+console.log(Math.random());  //value always in b/w 0-1
+console.log(Math.random()*10); 
+console.log((Math.random()*10) + 1);  //guarantees the value will not be zero //min value will be 1
+
+
+const min = 10;
+const max = 20;
+
+
+console.log(Math.floor(Math.random() * (max - min + 1))+ min);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
